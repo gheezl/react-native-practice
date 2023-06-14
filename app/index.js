ae29ff7914b68fb9d182666206c2ec729ff54335
Jacob Rushlow <gheezl25@gmail.com>
2023-06-14 13:44:14 -0400

    Continuing set up

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, SIZES, icons, images } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBt, Welcomd } from "../components";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
