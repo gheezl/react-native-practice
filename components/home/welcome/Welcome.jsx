b38ba773c046fb988b7b466f8a36d9878934fe82
Jacob Rushlow <gheezl25@gmail.com>
2023-06-15 11:09:39 -0400

    Building out the welcome page and _layout.js

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
 
-import styles from './welcome.style'
+import { useRouter } from 'expo-router';
+
+import styles from "./welcome.style";
+
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container} >
+        <Text style={styles.userName} >Hello Jacob</Text>
+        <Text style={styles.welcomeMessage} >Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer} >
+        <View style={styles.searchWrapper} >
+          <TextInput
+            style={styles.searchInput}
+            value={searchValue}
+            onChange={e => setSearchValue(e.target.value)}
+            placeholder='What job are you looking for?'
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn}>
+          <Image
+            style={styles.searchBtnImage}
+            source={icons.search}
+            resizeMode='contain'
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
-export default Welcome
+export default Welcome;
