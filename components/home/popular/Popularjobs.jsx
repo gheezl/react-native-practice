10019b29183dd15177c4b7ac2d7eb56fadfbcd39
Jacob Rushlow <gheezl25@gmail.com>
2023-06-15 15:25:37 -0400

    Building out the Popularjobs page

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 
-import styles from './popularjobs.style'
+import styles from './popularjobs.style';
+
+import { COLORS, SIZES } from "../../../constants";
+
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import { useRouter } from 'expo-router';
+import { isLoading } from 'expo-font';
 
 const Popularjobs = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn} >Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer} >
+        {
+          isLoading
+            ? <ActivityIndicator size="large" colors={COLORS.primary} />
+            : error
+              ? <Text>Something went wrong :(</Text>
+              :
+              <FlatList
+                data={[2, 3, 35, 454, 46, 47, 56, 5,]}
+                renderItem={({ item }) => (
+                  <PopularJobCard item={item} />
+                )}
+                keyExtractor={item => item}
+              />
+        }
+      </View>
     </View>
   )
 }
